6d4ea82dfa4a998cf9cb5ddec736a43f529c0ebe
Endel Dreyer <endel.dreyer@gmail.com>
2015-12-12 16:01:50 +0000

    add colyseus client and ammo physics library

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
 import Stage from './tools/Stage';
+import Colyseus from 'colyseus.js'
+
+var colyseus = new Colyseus("ws://localhost:3553")
+
+console.log(Ammo)
+
+var room = colyseus.join('map1')
+room.on('setup', function(data) {
+  console.log("initial data!", data)
+})
+room.on('patch', function(patches) {
+  console.log("patched ", patches)
+})
 
 var stage = new Stage({
   targetWidth: 800,
