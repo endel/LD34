fe004bdd0fce0428e4d49b67bed68a4444b837b6
Endel Dreyer <endel.dreyer@gmail.com>
2015-12-12 22:53:06 +0000

    fix merge

--- src/game/Player.js
+++ src/game/Player.js
@@ -31,6 +31,6 @@ export default class Player extends Entity {
   update(delta) {
     this.position.x = lerp(this.position.x, this.targetX, this.ease);
     this.position.y = lerp(this.position.y, this.targetY, this.ease);
-    this.rotation = lerp(this.rotation || 0, this.targetAngle, this.ease);
+    this.entity.rotation = lerp(this.entity.rotation || 0, this.targetAngle, this.ease);
   }
 }
