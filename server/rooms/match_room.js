9f05900b2c6d5749522feb477a5a24f668be3374
Endel Dreyer <endel.dreyer@gmail.com>
2015-12-12 17:11:38 +0000

    simple physics simulation working

--- server/rooms/match_room.js
+++ server/rooms/match_room.js
@@ -2,7 +2,7 @@
 
 var Room = require('colyseus').Room
   , ClockTimer = require('clock-timer.js')
-  , Ammo = require('../../ammo')
+  , Simulation = require('../simulation')
 
 const TICK_RATE = 30
     , PATCH_RATE = 20
@@ -18,6 +18,7 @@ class MatchRoom extends Room {
     })
 
     this.playerProps = {}
+    this.simulation = new Simulation()
 
     this.clock = new ClockTimer()
     this.tickInterval = setInterval(this.tick.bind(this), 1000 / TICK_RATE)
@@ -28,16 +29,20 @@ class MatchRoom extends Room {
   }
 
   onJoin (client, options) {
+    var x = 10
+      , y = 10
+
     this.state.players[ client.id ] = {
-      x: 0,
-      y: 0,
+      x: x,
+      y: y,
       angle: 0
     }
 
     this.playerProps[client.id] = {
       left: 0,
       right: 0,
-      speed: 0
+      speed: 0,
+      body: this.simulation.add(x, y)
     }
 
     this.sendState(client)
@@ -58,6 +63,7 @@ class MatchRoom extends Room {
   tick () {
     // update game logic
     this.clock.tick()
+    this.simulation.simulate(this.clock.deltaTime)
 
     for (var clientId in this.playerProps) {
       this.updatePlayer(clientId, this.playerProps[clientId])
@@ -69,6 +75,20 @@ class MatchRoom extends Room {
   }
 
   updatePlayer (clientId, props) {
+    // Get data from physics
+    var body = props.body
+      , transform = new Ammo.btTransform(); // TODO: reuse this instance to reduce leaking
+
+    body.getMotionState().getWorldTransform(transform);
+
+    var origin = transform.getOrigin();
+    var rotation = transform.getRotation();
+    this.state.players[clientId].x = origin.x()
+    this.state.players[clientId].y = origin.y()
+    this.state.players[clientId].angle = rotation.z()
+
+    console.log("Update player state: ", origin.x(), origin.y(), rotation.z())
+
     if (props.left === 1) {
       // accelerate left
     }
---
+++ server/simulation.js
@@ -0,0 +1,48 @@
+// var Module = { TOTAL_MEMORY: 256*1024*1024 };
+var Ammo = require('../ammo')
+
+class Simulation {
+
+  constructor () {
+
+    this.transform = new Ammo.btTransform(); // taking this out of readBulletObject reduces the leaking
+
+    // Bullet-interfacing code
+    this.collisionConfiguration = new Ammo.btDefaultCollisionConfiguration();
+    this.dispatcher = new Ammo.btCollisionDispatcher(this.collisionConfiguration);
+    this.overlappingPairCache = new Ammo.btDbvtBroadphase();
+    this.solver = new Ammo.btSequentialImpulseConstraintSolver();
+
+    this.dynamicsWorld = new Ammo.btDiscreteDynamicsWorld(this.dispatcher, this.overlappingPairCache, this.solver, this.collisionConfiguration);
+    this.dynamicsWorld.setGravity(new Ammo.btVector3(0, -10, 0));
+
+    this.bodies = []
+  }
+
+  add (startX, startY) {
+    var groundTransform = new Ammo.btTransform();
+    groundTransform.setIdentity();
+    groundTransform.setOrigin(new Ammo.btVector3(0, -56, 0));
+
+    var mass = 1;
+    var localInertia = new Ammo.btVector3(0, 0, 0);
+    var boxShape = new Ammo.btBoxShape(new Ammo.btVector3(1, 1, 1));
+    boxShape.calculateLocalInertia(mass, localInertia);
+
+    var myMotionState = new Ammo.btDefaultMotionState(groundTransform);
+    var rbInfo = new Ammo.btRigidBodyConstructionInfo(mass, myMotionState, boxShape, localInertia);
+    var body = new Ammo.btRigidBody(rbInfo);
+
+    this.dynamicsWorld.addRigidBody(body);
+    this.bodies.push(body);
+
+    return body
+  }
+
+  simulate (deltaTime) {
+    this.dynamicsWorld.stepSimulation(deltaTime || 1, 2);
+  }
+
+}
+
+module.exports = Simulation
