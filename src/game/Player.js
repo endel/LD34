f2517f11606eb5234f99a9c5b1d84e25aeb0aaf1
Endel Dreyer <endel.dreyer@gmail.com>
2015-12-12 22:17:21 +0000

    fix lerping rotation

--- src/game/Player.js
+++ src/game/Player.js
@@ -5,6 +5,10 @@ export default class Player extends Entity {
 
   constructor() {
     super(0xFFFF00, 32, 40);
+
+    this.targetX = this.position.x
+    this.targetY = this.position.y
+    this.targetAngle = 0
   }
 
   set x (targetX) { this.targetX = targetX }
@@ -14,7 +18,7 @@ export default class Player extends Entity {
   update(delta) {
     this.position.x = lerp(this.position.x, this.targetX, 0.2)
     this.position.y = lerp(this.position.y, this.targetY, 0.2)
-    this.rotation = lerp(this.rotation, this.targetAngle, 0.2)
+    this.rotation = lerp(this.rotation || 0, this.targetAngle, 0.2)
   }
 
 }
